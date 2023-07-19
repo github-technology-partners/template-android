864c3dd10e35156d387b3b2c5699796110d0f501
sneha-natekar <58860302+sneha-natekar@users.noreply.github.com>
2023-07-18 17:32:45 -0700

    Update android-accept-licenses.sh

--- tools/android-accept-licenses.sh
+++ tools/android-accept-licenses.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/expect -f
 
+# Set the timeout to 1800 seconds (30 minutes)
 set timeout 1800
+
+# Get the command and licenses from command-line arguments
 set cmd [lindex $argv 0]
 set licenses [lindex $argv 1]
 
+# Start a new process with the provided command
 spawn {*}$cmd
+
+# Expect the pattern "(y/N)" in the process output
 expect {
-  "(y/N)" {
+    # If the pattern is found, automatically send "y" followed by a carriage return (\r)
+    "(y/N)" {
         exp_send "y\r"
+        # Continue to wait for the pattern in case it appears again
         exp_continue
-  }
-  eof
+    }
+    # If the process has finished, stop waiting and continue with the script
+    eof
 }
