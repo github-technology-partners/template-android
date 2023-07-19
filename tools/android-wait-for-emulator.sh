292a5830fb2b56d8ad3a8e4439763fed469ce302
sneha-natekar <58860302+sneha-natekar@users.noreply.github.com>
2023-07-18 17:36:27 -0700

    Update android-wait-for-emulator.sh

--- tools/android-wait-for-emulator.sh
+++ tools/android-wait-for-emulator.sh
@@ -2,29 +2,33 @@
 
 set +e
 
-bootcomplete=""
-failcounter=0
 timeout=600
 sleeptime=10
 maxfail=$((timeout / sleeptime))
 
-until [[ "${bootcomplete}" =~ "1" ]]; do
-    bootcomplete=`adb -e shell getprop dev.bootcomplete 2>&1 &`
-    if [[ "${bootcomplete}" =~ "" ]]; then
-        ((failcounter += 1))
-        echo "Waiting for emulator to start"
-        if [[ ${failcounter} -gt ${maxfail} ]]; then
-            echo "Timeout ($timeout seconds) reached; failed to start emulator"
-            while pkill -9 "emulator" >/dev/null 2>&1; do
-                echo "Killing emulator process...."
-                pgrep "emulator"
-            done
-            echo "Process terminated"
-            pgrep "emulator"
-            exit 1
-        fi
-    fi
-    sleep ${sleeptime}
-done
+wait_for_emulator_start() {
+    local bootcomplete=""
+    local failcounter=0
 
-echo "Emulator is ready"
+    while [[ $failcounter -le $maxfail ]]; do
+        bootcomplete=$(adb -e shell getprop dev.bootcomplete 2>&1)
+        if [[ "$bootcomplete" == "1" ]]; then
+            echo "Emulator is ready"
+            return 0
+        fi
+        ((failcounter++))
+        echo "Waiting for emulator to start"
+        sleep "$sleeptime"
+    done
+
+    echo "Timeout ($timeout seconds) reached; failed to start emulator"
+    while pkill -9 "emulator" >/dev/null 2>&1; do
+        echo "Killing emulator process..."
+        pgrep "emulator"
+    done
+    echo "Process terminated"
+    pgrep "emulator"
+    return 1
+}
+
+wait_for_emulator_start
